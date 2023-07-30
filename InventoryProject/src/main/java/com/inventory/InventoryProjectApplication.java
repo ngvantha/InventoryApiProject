6619e5137613857340e580cd14b76b42d31db9fa
ngvtha <nguyentha.jp@gmail.com>
2023-07-30 19:42:37 +0900

    tắt chế độ tự động boot cònig của hibernate

--- InventoryProject/src/main/java/com/inventory/InventoryProjectApplication.java
+++ InventoryProject/src/main/java/com/inventory/InventoryProjectApplication.java
@@ -1,9 +1,18 @@
 package com.inventory;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+//Disable some Spring Boot auto config
+@EnableAutoConfiguration(exclude = { 						//
+     DataSourceAutoConfiguration.class, 					//
+     DataSourceTransactionManagerAutoConfiguration.class, 	//
+     HibernateJpaAutoConfiguration.class })					//
 public class InventoryProjectApplication {
 
 	public static void main(String[] args) {
