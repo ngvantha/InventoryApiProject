4d3fefff27c7f15e5b89c62520eccfd85f1e62ff
ngvtha <nguyentha.jp@gmail.com>
2023-08-01 21:43:21 +0900

    fix unit repository

--- InventoryProject/src/main/java/com/inventory/db1/repositories/IUnitRepository.java
+++ InventoryProject/src/main/java/com/inventory/db1/repositories/IUnitRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.inventory.db1.entities.Unit;
 
 public interface IUnitRepository extends JpaRepository<Unit, Integer> {
-	public Unit findByName(String name);
-
-	public boolean existsByName(String name);
+//	public Unit findByName(String name);
+//
+//	public boolean existsByName(String name);
 }
